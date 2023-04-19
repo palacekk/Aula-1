ba4521562a93e28657226c4283d83e06e8b8f485
palacekk <129327413+palacekk@users.noreply.github.com>
2023-04-19 17:47:44 -0300

    aula1

---
+++ src/public/aula1.php
@@ -0,0 +1,30 @@
+<?php
+      /*$time = array('Maria', 'José', 'Ana');
+      echo $time[2];
+      echo '<br />';
+      print_r($time);
+      */
+
+      /*echo 'a: [' . TRUE . ']<br />';
+      echo 'a: [' . FALSE . ']<br />';
+    
+      echo 'a: [' . (20 > 9) . ']<br />';
+      echo 'b: [' . (5 == 6) . ']<br />';
+      echo 'c: [' . (1 == 0) . ']<br />';
+      echo 'd: [' . (1 == 1) . ']<br />';
+      $nome = 'Maria';
+      echo 'e: [' . $nome . ']<br />';
+      */
+      $menu = 'sobre';
+      switch($menu){
+        case 'index':
+            echo 'Escolheu a página inicial.';
+            break;
+        case 'sobre':
+            echo 'Escolheu a página sobre.';
+            break;
+        
+      }
+
+    
+?>
